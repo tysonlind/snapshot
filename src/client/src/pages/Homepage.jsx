e8069b7fb33ae26b560cc8ec25dbc048fe859507
basagoitiam <marika.basagoitia@gmail.com>
2023-07-07 10:34:29 -0400

    changed fetch url

--- src/client/src/pages/Homepage.jsx
+++ src/client/src/pages/Homepage.jsx
@@ -77,9 +77,7 @@ function Homepage() {
         setSavedSearches([...updatedSavedSearches]);
     }
   useEffect(() => {
-    fetch(`http://localhost:${port}` + "?" + new URLSearchParams({
-      query: query
-    }))
+    fetch(`https://snapshot-photo-search-d3bc5642cb86.herokuapp.com/`)
       .then((res) => res.json())
       .then((data) => {
         if (data.length > 0) {
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import apiRouter from "./routes";
 import router from "./routes";
 import config from "./config";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -9,33 +8,15 @@ import { join } from "path";
 
 const app = express();
 
-/**
- * Parses incoming request body as json if header indicates application/json
- */
 app.use(express.json());
 
-/**
- * Enables incoming requests from cross origin domains
- */
 app.use(cors());
 
-/**
- * Logs incoming request information to the dev console
- */
 app.use(morgan("dev"));
 
-/**
- * Directs incoming static asset requests to the public folder
- */
-
-app.use(express.static(join(__dirname, "../client/build")));
+//app.use(express.static(join(__dirname, "../client/build")));
 
 app.use(router);
-
-/**
- * Sends the react app index.html for page requests
- * Only needed in production when you are not using the react dev server
- */
 
 app.use((req, res, next) => {
   try {
@@ -45,15 +26,8 @@ app.use((req, res, next) => {
   }
 });
 
-/**
- * Error handler middleware
- */
 app.use(errorHandler);
 
-/**
- * Bind the app to a specified port
- * You can access your app at http://localhost:<port>
- */
 app.listen(config.port || 8080, () =>
   console.log(`Server listening on port ${config.port}...`)
 );
